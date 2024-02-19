5370eab38e8a14700661ea11ca7fa8387cf68f32
065-SachinSharma-ECEA-DSLAB <159753918+065-SachinSharma-ECEA-DSLAB@users.noreply.github.com>
2024-02-19 18:48:06 +0530

    Add files via upload
    static menu driven code  practical1

---
+++ p1.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+
+void linear_search(int arr[], int n, int element) {
+    
+    for (int i = 0; i<n; i++) {
+        if (arr[i] == element) {
+            printf("Element %d found at index %d.\n", element, i);
+            return; // Exit the function after finding the element
+        }
+    }
+    printf("Element %d not found.\n", element);
+}
+
+void insert_element(int arr[], int n, int index, int element) {
+    for(int i=n-1;i>=index;i--){
+        //shift forward
+        arr[i+1]=arr[i];
+    }
+    arr[index]=element;
+    n++;
+}
+
+void delete_element(int arr[], int n, int index) {
+    for(int i=index;i<=n-1;i++){
+        //shift backward
+        arr[i]=arr[i+1];
+    }
+    n--;
+}
+
+void reverse_array(int arr[], int n) {
+    for (int i = 0; i< n/2; i++) {
+        // Swap arr[i] with arr[n-i-1]
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+}
+
+void update_array(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            arr[i] += 5;
+        } else {
+            arr[i] *= 2;
+        }
+    }
+}
+void print_array(int arr[], int n) {
+    printf("Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main() {
+    int choice, n, i, index,element;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (i = 0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Linear Search\n");
+        printf("2. Insert Element\n");
+        printf("3. Delete Element\n");
+        printf("4. Reverse Array\n");
+        printf("5. Update Array\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                
+                printf("Enter the element to search: ");
+                scanf("%d", &element);
+                linear_search(arr, n,element );
+                
+                break;
+            case 2:
+                printf("enter the index of array to insert element ");
+                scanf("%d",&index);
+                printf("enter the element to be inserted ");
+                scanf("%d",&element);
+                insert_element(arr,n,index,element);
+                printf("\nafter inserion aray : ");
+                for(int i=0;i<=n;i++){
+                    printf("%d ",arr[i]);
+                }
+                break;
+            case 3:
+                printf("enter the index of element to be deleted ");
+                scanf("%d",&index);
+                delete_element(arr,n,index);
+                printf("\nafter deletion : ");
+                for(int i=0;i<n-1;i++){
+                    printf("%d ",arr[i]);
+                } 
+                break;
+            case 4:
+                reverse_array(arr, n);
+                print_array(arr, n); 
+                break;
+            case 5:
+                update_array(arr, n);
+                print_array(arr, n); 
+                break;
+            case 6:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Please enter a number between 1 and 6.\n");
+        }
+        
+    } while (choice != 6);
+
+    return 0;
+}
